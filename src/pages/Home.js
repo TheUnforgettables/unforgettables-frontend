978a527054111c2d151ba3e03a3be7ad839a8be7
Garry Greathouse <garry.greathouse@gmail.com>
2024-03-11 18:28:34 -0700

    fixed home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,5 @@
 import React from "react"
 import Video from "../assets/video1.mp4"
-import Sheet from "@mui/joy/Sheet"
-
-import { Typography } from "@mui/material"
 
 const Home = () => {
   return (
@@ -11,26 +8,10 @@ const Home = () => {
         <source src={Video} type="video/mp4" />
       </video>
       <div className="overlay">
-        <Sheet
-          className="card-holder"
-          color="plain"
-          variant="solid"
-          style={{ top: "-160px" }}
-        >
-          <Typography level="h1" className="heading">
-            <h1>Welcome to Unforgettable:</h1>
-          </Typography>
-          <Typography level="h2" className="heading">
-            <h2>
-              The ultimate digital heirloom for your family's culinary journey.
-              Celebrate the flavors, stories, and memories that make your
-              family's kitchen truly special with Unforgettable. Start building
-              your delicious legacy today!
-            </h2>
-          </Typography>
-        </Sheet>
+        <h1 className="title">Welcome to Unforgettable: the ultimate digital heirloom for your family's culinary journey. Celebrate the flavors, stories, and memories that make your family's kitchen truly special with Unforgettable. Start building your delicious legacy today!
+        </h1>
       </div>
-    </div>
+   </div>
   )
 }
 
